affa8ca9b29d27f1de008b0ac164a129690f313e
SITRAKA RAJAONARIVELO <110409327+louft-kmth@users.noreply.github.com>
2023-02-07 14:42:11 +0100

    Update streamlite.py

--- streamlite.py
+++ streamlite.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1VSTAV0eM6QAe5G7D7f06bnLOoxFSc70K
 """
 
-pip install streamlit
-
 import streamlit as st
 
 import pandas as pd
